25acb8d35cf511b42b060c75f5352060d58af33a
Matheus M. <matheusmoraesdj2025@gmail.com>
2025-08-15 22:22:24 -0300

    Create utils.js

---
+++ docs/utils.js
@@ -0,0 +1,8 @@
+// Funções utilitárias
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function clamp(value, min, max) {
+  return Math.min(Math.max(value, min), max);
+}
